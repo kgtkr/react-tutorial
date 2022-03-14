27d1f913b7762cc0479c449139835d55a676ab4b
kgtkr <contact@kgtkr.net>
2022-03-14 23:01:12 +0900

    「State のリフトアップ、再び」

--- src/Board.tsx
+++ src/Board.tsx
@@ -2,70 +2,32 @@ import * as React from "react";
 import Square, { SquareValue } from "./Square";
 import styles from "./Board.module.scss";
 
-export type Props = {};
+export type Props = {
+  squares: SquareValue[];
+  onClick?: (i: number) => void;
+};
 
-function Board({}: Props) {
-  const [squares, setSquares] = React.useState<SquareValue[]>(
-    Array<SquareValue>(9).fill(null)
-  );
-  const [xIsNext, setXIsNext] = React.useState<boolean>(true);
-
-  const handleClick = (i: number) => () => {
-    if (calculateWinner(squares) !== null || squares[i] !== null) {
-      return;
-    }
-    const newSquares = [...squares];
-    newSquares[i] = xIsNext ? "X" : "O";
-    setSquares(newSquares);
-    setXIsNext(!xIsNext);
-  };
-
-  const winner = calculateWinner(squares);
-  const status =
-    winner !== null
-      ? `Winner: ${winner}`
-      : `Next player: ${xIsNext ? "X" : "O"}`;
-
+function Board({ squares, onClick }: Props) {
   return (
     <div>
       <div className={styles.status}>{status}</div>
       <div className={styles.boardRow}>
-        <Square value={squares[0]} onClick={handleClick(0)} />
-        <Square value={squares[1]} onClick={handleClick(1)} />
-        <Square value={squares[2]} onClick={handleClick(2)} />
+        <Square value={squares[0]} onClick={() => onClick?.(0)} />
+        <Square value={squares[1]} onClick={() => onClick?.(1)} />
+        <Square value={squares[2]} onClick={() => onClick?.(2)} />
       </div>
       <div className={styles.boardRow}>
-        <Square value={squares[3]} onClick={handleClick(3)} />
-        <Square value={squares[4]} onClick={handleClick(4)} />
-        <Square value={squares[5]} onClick={handleClick(5)} />
+        <Square value={squares[3]} onClick={() => onClick?.(3)} />
+        <Square value={squares[4]} onClick={() => onClick?.(4)} />
+        <Square value={squares[5]} onClick={() => onClick?.(5)} />
       </div>
       <div className={styles.boardRow}>
-        <Square value={squares[6]} onClick={handleClick(6)} />
-        <Square value={squares[7]} onClick={handleClick(7)} />
-        <Square value={squares[8]} onClick={handleClick(8)} />
+        <Square value={squares[6]} onClick={() => onClick?.(6)} />
+        <Square value={squares[7]} onClick={() => onClick?.(7)} />
+        <Square value={squares[8]} onClick={() => onClick?.(8)} />
       </div>
     </div>
   );
 }
 
 export default Board;
-
-function calculateWinner(squares: SquareValue[]): SquareValue {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
--- src/Game.tsx
+++ src/Game.tsx
@@ -1,17 +1,49 @@
 import * as React from "react";
 import Board from "./Board";
 import styles from "./Game.module.scss";
+import { SquareValue } from "./Square";
 
 export type Props = {};
 
 function Game({}: Props) {
+  const [history, setHistory] = React.useState<
+    {
+      squares: SquareValue[];
+    }[]
+  >([
+    {
+      squares: Array<SquareValue>(9).fill(null),
+    },
+  ]);
+  const [xIsNext, setXIsNext] = React.useState<boolean>(true);
+
+  const handleClick = (i: number) => {
+    const current = history[history.length - 1];
+    const squares = current.squares;
+
+    if (calculateWinner(squares) !== null || squares[i] !== null) {
+      return;
+    }
+    const newSquares = [...squares];
+    newSquares[i] = xIsNext ? "X" : "O";
+    setHistory([...history, { squares: newSquares }]);
+    setXIsNext(!xIsNext);
+  };
+
+  const current = history[history.length - 1];
+  const winner = calculateWinner(current.squares);
+  const status =
+    winner !== null
+      ? `Winner: ${winner}`
+      : `Next player: ${xIsNext ? "X" : "O"}`;
+
   return (
     <div className={styles.game}>
       <div>
-        <Board />
+        <Board squares={current.squares} onClick={handleClick} />
       </div>
       <div className={styles.gameInfo}>
-        <div>{/* status */}</div>
+        <div>{status}</div>
         <ol>{/* TODO */}</ol>
       </div>
     </div>
@@ -19,3 +51,23 @@ function Game({}: Props) {
 }
 
 export default Game;
+
+function calculateWinner(squares: SquareValue[]): SquareValue {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
