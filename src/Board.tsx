ced53a8c36b117c5ee7dba7ee01b62d5e49a47c0
kgtkr <contact@kgtkr.net>
2022-03-14 22:42:39 +0900

    「ゲーム勝者の判定」

--- src/Board.tsx
+++ src/Board.tsx
@@ -11,13 +11,20 @@ function Board({}: Props) {
   const [xIsNext, setXIsNext] = React.useState<boolean>(true);
 
   const handleClick = (i: number) => () => {
+    if (calculateWinner(squares) !== null || squares[i] !== null) {
+      return;
+    }
     const newSquares = [...squares];
     newSquares[i] = xIsNext ? "X" : "O";
     setSquares(newSquares);
     setXIsNext(!xIsNext);
   };
 
-  const status = "Next player: X";
+  const winner = calculateWinner(squares);
+  const status =
+    winner !== null
+      ? `Winner: ${winner}`
+      : `Next player: ${xIsNext ? "X" : "O"}`;
 
   return (
     <div>
@@ -42,3 +49,23 @@ function Board({}: Props) {
 }
 
 export default Board;
+
+function calculateWinner(squares: SquareValue[]): SquareValue {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
